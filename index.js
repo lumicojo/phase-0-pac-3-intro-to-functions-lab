b8a9033624b2b74675be6caace19c9f3b0050cc2
+ <+>
2022-09-03 11:50:46 -0400

    commit tree b399f30f1ad55ed2d14ae94c8e2ce8b71eb0822a

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
-
-"Hello!".toUpperCase(); // 'HELLO!'
 function shout(string) {
-    return string.toUpperCase();
-  }
-
-"Hello".toLowerCase();
+    return "Hello".toUpperCase();
+}
 function whisper(string) {
-    return string.toLowerCase();
+    return "hello".toLowerCase();
 }
-
 function logShout(string) {
-    console.log(string.toUpperCase());
-}
+    console.log("hello".toUpperCase());
+} 
+function  logWhisper(string) {
+    console.log("hello".toLowerCase());
+}  
 
-function logWhisper(string) {
-    console.log(string.toLowerCase());
-}
-
-function sayHiToHeadphonedRoommate(string) {
-    if (string.toLowerCase() === string) { 
-        return("I can\'t hear you!");
-    } else if (string.toUpperCase() === string) {
-        return("YES INDEED!");
+function sayHiToHeadphonedRoommate(string) { 
+    if (string === string.toLowerCase()) {
+        return "I can't hear you!"
+    } else if (string === string.toUpperCase()) {
+        return "YES INDEED!"
     } else if (string === "Let's have dinner together!") {
         return "I would love to!"
-    }    
-}
-    
+    }
+
+  }
+  
+  
+  
+  
+  
